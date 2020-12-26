4fabf93387b65f74b31b3bd19bf9244c4dd3304c
Satvik Dandale <45892894+SatvikDandale@users.noreply.github.com>
2020-12-26 21:59:11 +0530

    Email to college domain API

--- src/main/java/com/campussocialmedia/campussocialmedia/controllers/CollegeDomainNameController.java
+++ src/main/java/com/campussocialmedia/campussocialmedia/controllers/CollegeDomainNameController.java
@@ -17,71 +17,62 @@ import com.campussocialmedia.campussocialmedia.service.CollegeDomainNameService;
 
 @RestController
 public class CollegeDomainNameController {
-	
+
 	@Autowired
 	private CollegeDomainNameService service;
-	
-	
-	//If a college registers on the platform then add its domain name in database
-	//if domain name is vit.edu, then "@vit.edu" will be added to database
+
+	// If a college registers on the platform then add its domain name in database
+	// if domain name is vit.edu, then "@vit.edu" will be added to database
 	@PostMapping("/addDomainName")
 	public ResponseEntity<?> saveDomainName(@RequestBody CollegeDomainNames domainName) {
 		System.out.println("in /addDomainName");
 		try {
 			CollegeDomainNames domainNameObj = service.addDomainName(domainName);
-			
-			return new ResponseEntity<>(domainNameObj,HttpStatus.OK);
-		}
-		catch(Exception e) {
-			ExceptionResponse exceptionResponse = new ExceptionResponse(
-             new Date() , "Something Went Wrong " + domainName + " not found", "Some Details"
-			);
-			
-			return new ResponseEntity<>(exceptionResponse,HttpStatus.NOT_FOUND);
-			}
-	}
-	
-	//verifies if a college exists with the domain name
-	@GetMapping("/college/{domainName}")
-	public ResponseEntity<?> findCollege(@PathVariable String domainName)
-	{
-		//there is a problem here. Exception handling isn't working
-		try {
-			CollegeDomainNames domainNameObj = service.getCollegeByDomainName(domainName);
-			System.out.println(domainNameObj);
-			return new ResponseEntity<>(domainNameObj,HttpStatus.OK);
-		}
-		catch(Exception e) {
-			ExceptionResponse exceptionResponse = new ExceptionResponse(
-             new Date() , "College with DomainName: " + domainName + " not found", "Some Details"
-			);
-			
-			return new ResponseEntity<>(exceptionResponse,HttpStatus.NOT_FOUND);
+
+			return new ResponseEntity<>(domainNameObj, HttpStatus.OK);
+		} catch (Exception e) {
+			ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),
+					"Something Went Wrong " + domainName + " not found", "Some Details");
+
+			return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
 		}
 	}
-	
+
+	// verifies if a college exists with the domain name
+	// @GetMapping("/college/{domainName}")
+	// public ResponseEntity<?> findCollege(@PathVariable String domainName) {
+	// 	// there is a problem here. Exception handling isn't working
+	// 	try {
+	// 		CollegeDomainNames domainNameObj = service.getCollegeByDomainName(domainName);
+	// 		System.out.println(domainNameObj);
+	// 		return new ResponseEntity<>(domainNameObj, HttpStatus.OK);
+	// 	} catch (Exception e) {
+	// 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),
+	// 				"College with DomainName: " + domainName + " not found", "Some Details");
+
+	// 		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+	// 	}
+	// }
+
 	/*
-	 * it takes emailId and extracts domain name from it. then verifies if any college exists in the database with the domain name
+	 * it takes emailId and extracts domain name from it. then verifies if any
+	 * college exists in the database with the domain name
+	 */
 	@GetMapping("/college/{emailId}")
-	public ResponseEntity<?> findCollegeByEmailId(@PathVariable String emailId){
-		
+	public ResponseEntity<?> findCollegeByEmailId(@PathVariable String emailId) {
+
 		try {
-				String[] arrOfStr = emailId.split("@");
-				String domainName = arrOfStr[arrOfStr.length];
-				System.out.println(domainName);
-				CollegeDomainNames domainNameObj = service.getCollegeByDomainName(domainName);
-				return new ResponseEntity<>(domainNameObj.getCollegeName(),HttpStatus.OK);
-				
+			String[] arrOfStr = emailId.split("@");
+			String domainName = arrOfStr[arrOfStr.length];
+			System.out.println(domainName);
+			CollegeDomainNames domainNameObj = service.getCollegeByDomainName(domainName);
+			return new ResponseEntity<>(domainNameObj.getCollegeName(), HttpStatus.OK);
+
+		} catch (Exception e) {
+			ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),
+					"College with DomainName for this email id " + emailId + " not found", "Some Details");
+			return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
 		}
-		catch(Exception e) {
-			ExceptionResponse exceptionResponse = new ExceptionResponse(
-		             new Date() , "College with DomainName for this email id " + emailId + " not found", "Some Details"
-					);
-			return new ResponseEntity<>(exceptionResponse,HttpStatus.NOT_FOUND);
-		}
-		
-		
 	}
-	*/
 
 }
